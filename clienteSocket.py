df8f66e097f9d189abe334811505d6f599940a4b
Jack625a <ktfut15@gmail.com>
2026-04-20 09:50:13 -0400

    Socket Cliente

---
+++ clienteSocket.py
@@ -0,0 +1,33 @@
+#Importar las librerias
+import socket
+import threading
+
+host=""
+puerto=5000
+
+def recibirMensajes(cliente):
+    while True:
+        try:
+            mensaje=cliente.recv(1024).decode("utf-8")
+            print(mensaje)
+        except:
+            print("Error al recibir el mensaje")
+            cliente.close()
+            break
+
+def enviarMensajes(cliente):
+    while True:
+        mensajes=input("Ingrese su mensaje: ")
+        cliente.send(mensajes.encode("utf-8"))
+
+def inicioCliente():
+    cliente=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    cliente.connect((host,puerto))
+    print("Conectado al chat...")
+
+    hiloRecibir=threading.Thread(target=recibirMensajes,args=(cliente,))
+    hiloRecibir.start()
+
+    enviarMensajes(cliente)
+
+inicioCliente()
